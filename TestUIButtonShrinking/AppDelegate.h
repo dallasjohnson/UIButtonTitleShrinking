8f62dfcb7c0206d8b90eb404b5c972e9cc8f79cb
Dallas Johnson <dallasj001@gmail.com>
2015-08-18 17:46:16 +0100

    Initial Commit

---
+++ TestUIButtonShrinking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestUIButtonShrinking
+//
+//  Created by Dallas Johnson on 18/08/2015.
+//  Copyright (c) 2015 Dallas Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestUIButtonShrinking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUIButtonShrinking
+//
+//  Created by Dallas Johnson on 18/08/2015.
+//  Copyright (c) 2015 Dallas Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
